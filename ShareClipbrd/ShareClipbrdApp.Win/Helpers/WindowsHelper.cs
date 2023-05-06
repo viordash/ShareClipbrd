d20517ceca7117032a420254ad801212b1765f01
WIN-U2EPC47INN1\user <viordash@mail.ru>
2023-05-06 23:41:05 +0300

    setup window position

---
+++ ShareClipbrd/ShareClipbrdApp.Win/Helpers/WindowsHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace ShareClipbrdApp.Win.Helpers {
+    public class WindowsHelper {
+        public static void LoadLocation(System.Drawing.Point point, Window window) {
+            if(!point.IsEmpty
+                && point.X + window.Width >= 0
+                && point.Y + window.Height >= 0
+                && SystemParameters.FullPrimaryScreenWidth > point.X
+                && SystemParameters.FullPrimaryScreenHeight > point.Y) {
+                window.Left = point.X;
+                window.Top = point.Y;
+            }
+        }
+
+        public static void LoadSize(System.Drawing.Size size, Window window) {
+            if(!size.IsEmpty
+                && SystemParameters.FullPrimaryScreenWidth > size.Width
+                && SystemParameters.FullPrimaryScreenHeight > size.Height) {
+                window.Width = size.Width;
+                window.Height = size.Height;
+            }
+        }
+    }
+}
--- ShareClipbrd/ShareClipbrdApp.Win/MainWindow.xaml.cs
+++ ShareClipbrd/ShareClipbrdApp.Win/MainWindow.xaml.cs
@@ -1,25 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ShareClipbrdApp.Win.Helpers;
+using ShareClipbrdApp.Win.Properties;
 
 namespace ShareClipbrdApp.Win {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        public System.Drawing.Rectangle Bounds { get { return new System.Drawing.Rectangle((int)Left, (int)Top, (int)Width, (int)Height); } }
+
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, System.EventArgs e) {
+            WindowsHelper.LoadLocation(Settings.Default.MainFormLocation, this);
+            WindowsHelper.LoadSize(Settings.Default.MainFormSize, this);
+        }
+
+        private void Window_Closed(object sender, System.EventArgs e) {
+            Settings.Default.MainFormLocation = new System.Drawing.Point((int)Left, (int)Top);
+            Settings.Default.MainFormSize = Bounds.Size;
+            Settings.Default.Save();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            this.Close();
         }
     }
 }
