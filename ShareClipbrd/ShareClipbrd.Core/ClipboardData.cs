cdf51b5cfbc69e3438c441d151e93b95352a30a5
WIN-U2EPC47INN1\user <viordash@mail.ru>
2023-05-07 20:40:43 +0300

    ui. handle exceptions

--- ShareClipbrd/ShareClipbrd.Core/ClipboardData.cs
+++ ShareClipbrd/ShareClipbrd.Core/ClipboardData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShareClipbrd.Core {
+﻿namespace ShareClipbrd.Core {
     public class ClipboardData {
         public Dictionary<string, object>? Formats { get; set; }
 
--- ShareClipbrd/ShareClipbrdApp.Win/App.xaml.cs
+++ ShareClipbrd/ShareClipbrdApp.Win/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Threading;
 using Microsoft.Extensions.DependencyInjection;
+using ShareClipbrd.Core.Services;
 
 namespace ShareClipbrdApp.Win {
     /// <summary>
@@ -9,6 +11,9 @@ namespace ShareClipbrdApp.Win {
     public partial class App : Application {
         readonly IServiceProvider serviceProvider;
         App() {
+
+            var currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += CurrentDomain_UnhandledException;
             serviceProvider = Win.Startup.BuildServiceProvider();
             InitializeComponent();
         }
@@ -16,6 +21,19 @@ namespace ShareClipbrdApp.Win {
         private void OnStartup(object sender, StartupEventArgs e) {
             var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
             mainWindow.Show();
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
+            var ex = (Exception)e.ExceptionObject;
+            var dialogService = serviceProvider.GetRequiredService<IDialogService>();
+            dialogService.ShowError(ex);
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e) {
+            var ex = e.Exception;
+            var dialogService = serviceProvider.GetRequiredService<IDialogService>();
+            dialogService.ShowError(ex);
+            e.Handled = true;
         }
     }
 }
