f3ce0ed2610f972bf984976fd3bbfc488f8d1f94
WIN-U2EPC47INN1\user <viordash@mail.ru>
2023-05-28 12:55:06 +0300

    remove progress indication from DispatchService and use application dispatcher

--- ShareClipbrd/ShareClipbrdApp.Win/Services/DispatchService.cs
+++ ShareClipbrd/ShareClipbrdApp.Win/Services/DispatchService.cs
@@ -9,26 +9,20 @@ using ShareClipbrd.Core.Services;
 namespace ShareClipbrdApp.Win.Services {
     public class DispatchService : IDispatchService {
         public void ReceiveData(ClipboardData clipboardData) {
-            var mainWindow = Application.Current.MainWindow as MainWindow;
-            mainWindow!.Dispatcher.BeginInvoke(new Action(async () => {
-                await using(ProcessIndicator.Indicate(mainWindow, ProcessIndicator.Mode.Receive)) {
-                    var dataObject = new DataObject();
-                    clipboardData.Deserialize((f, o) => dataObject.SetData(f, o));
-                    if(dataObject.GetFormats().Any()) {
-                        Debug.WriteLine($"   *** formats: {string.Join(", ", dataObject.GetFormats())}");
-                        System.Windows.Clipboard.Clear();
-                        System.Windows.Clipboard.SetDataObject(dataObject);
-                    }
+            Application.Current.Dispatcher.BeginInvoke(new Action(() => {
+                var dataObject = new DataObject();
+                clipboardData.Deserialize((f, o) => dataObject.SetData(f, o));
+                if(dataObject.GetFormats().Any()) {
+                    Debug.WriteLine($"   *** formats: {string.Join(", ", dataObject.GetFormats())}");
+                    System.Windows.Clipboard.Clear();
+                    System.Windows.Clipboard.SetDataObject(dataObject);
                 }
             }));
         }
 
         public void ReceiveFiles(StringCollection files) {
-            var mainWindow = Application.Current.MainWindow as MainWindow;
-            mainWindow!.Dispatcher.BeginInvoke(new Action(async () => {
-                await using(ProcessIndicator.Indicate(mainWindow, ProcessIndicator.Mode.Receive)) {
-                    System.Windows.Clipboard.SetFileDropList(files);
-                }
+            Application.Current.Dispatcher.BeginInvoke(new Action(() => {
+                System.Windows.Clipboard.SetFileDropList(files);
             }));
         }
     }
