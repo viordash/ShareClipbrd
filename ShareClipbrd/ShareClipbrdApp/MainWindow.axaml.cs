7ad66c1e7ca8d58402cd88f33c4bed06896ac672
WIN-U2EPC47INN1\user <viordash@mail.ru>
2023-06-06 01:01:51 +0300

    start Avalonia UI project

---
+++ ShareClipbrd/ShareClipbrdApp/MainWindow.axaml.cs
@@ -0,0 +1,67 @@
+using System.Diagnostics;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
+using ShareClipbrd.Core.Services;
+
+namespace ShareClipbrdApp {
+    public partial class MainWindow : Window {
+        //bool mouseDownForWindowMoving = false;
+        PointerPoint? originalPoint;
+
+
+        public MainWindow() {
+            InitializeComponent();
+
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        void OnInitialized(object sender, System.EventArgs e) {
+            //WindowsHelper.LoadLocation(Settings.Default.MainFormLocation, this);
+            //Height = SystemParameters.FullPrimaryScreenHeight / 40;
+            //Width = SystemParameters.FullPrimaryScreenWidth / 40;
+            //dataServer.Start();
+            //edHostAddress.Text = Settings.Default.HostAddress;
+            //edPartnerAddress.Text = Settings.Default.PartnerAddress;
+        }
+
+        void OnClosed(object sender, System.EventArgs e) {
+            //dataServer.Stop();
+            //Settings.Default.MainFormLocation = new System.Drawing.Point((int)Left, (int)Top);
+            //Settings.Default.Save();
+        }
+
+        void OnActivated(object sender, System.EventArgs e) {
+            Border.BorderBrush = Brushes.Aqua;
+            Border.BorderThickness = new Thickness(2);
+            Opacity = 1;
+        }
+
+        void OnDeactivated(object sender, System.EventArgs e) {
+            Border.BorderBrush = Brushes.DarkGoldenrod;
+            Border.BorderThickness = new Thickness(1);
+            Opacity = 0.545;
+        }
+
+        void OnPointerPressedEvent(object? sender, PointerPressedEventArgs e) {
+            originalPoint = e.GetCurrentPoint(this);
+        }
+
+        private void OnPointerReleasedEvent(object? sender, PointerReleasedEventArgs e) {
+            originalPoint = null;
+        }
+
+        private void OnPointerMoved(object? sender, PointerEventArgs e) {
+            if(originalPoint == null) {
+                return;
+            }
+
+            PointerPoint currentPoint = e.GetCurrentPoint(this);
+            Position = new PixelPoint(Position.X + (int)(currentPoint.Position.X - originalPoint.Position.X),
+                Position.Y + (int)(currentPoint.Position.Y - originalPoint.Position.Y));
+        }
+    }
+}
