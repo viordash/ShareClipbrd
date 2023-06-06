4c81dfccea5bb3a4534243e966224a41ca2ae766
WIN-U2EPC47INN1\user <viordash@mail.ru>
2023-06-07 00:20:21 +0300

    wip. add DI

--- ShareClipbrd/ShareClipbrdApp/App.axaml.cs
+++ ShareClipbrd/ShareClipbrdApp/App.axaml.cs
@@ -1,19 +1,36 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using ShareClipbrd.Core.Services;
 
 namespace ShareClipbrdApp {
     public partial class App : Application {
+        IServiceProvider? serviceProvider;
+
         public override void Initialize() {
+            var currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
             AvaloniaXamlLoader.Load(this);
         }
 
         public override void OnFrameworkInitializationCompleted() {
+            serviceProvider = Startup.BuildServiceProvider();
+
             if(ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
-                desktop.MainWindow = new MainWindow();
+                desktop.MainWindow = serviceProvider?.GetRequiredService<MainWindow>();
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
+            var ex = (Exception)e.ExceptionObject;
+            var dialogService = serviceProvider?.GetRequiredService<IDialogService>();
+            dialogService?.ShowError(ex);
         }
     }
 }
