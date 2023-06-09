cd488b2a59bcde6da002b49f4e3e3ce07267796c
WIN-U2EPC47INN1\user <viordash@mail.ru>
2023-06-10 02:00:24 +0300

    receive FileDropList

--- ShareClipbrd/ShareClipbrdApp/Services/DispatchService.cs
+++ ShareClipbrd/ShareClipbrdApp/Services/DispatchService.cs
@@ -7,6 +7,7 @@ using Avalonia.Input;
 using Avalonia.Threading;
 using ShareClipbrd.Core.Clipboard;
 using ShareClipbrd.Core.Services;
+using static ShareClipbrd.Core.Clipboard.ClipboardData;
 
 namespace ShareClipbrdApp.Services {
     public class DispatchService : IDispatchService {
@@ -24,7 +25,10 @@ namespace ShareClipbrdApp.Services {
 
         public async void ReceiveFiles(StringCollection files) {
             await Dispatcher.UIThread.InvokeAsync(new Action(() => {
-                //Application.Current.Clipboard.SetFileDropList(files);
+                var dataObject = new DataObject();
+                dataObject.Set(Format.FileNames, files.OfType<string>().ToList());
+                Application.Current?.Clipboard?.ClearAsync();
+                Application.Current?.Clipboard?.SetDataObjectAsync(dataObject);
             }));
         }
     }
