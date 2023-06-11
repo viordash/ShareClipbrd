7148e514a367a01044c96d408a11fd848ca6eede
alordash <alordash@mail.ru>
2023-06-11 17:08:21 +0300

    Added tetris progressbar

---
+++ ShareClipbrd/ShareClipbrd.Core/Helpers/RandomArrayHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareClipbrd.Core.Helpers {
+	public class RandomArrayHelper {
+		public static void ShuffleArray(int[] arr, Random random) {
+			for (int idx = 0; idx < arr.Length; idx++) {
+				var randomIdx = random.Next(0, idx);
+				(arr[idx], arr[randomIdx]) = (arr[randomIdx], arr[idx]);
+			}
+		}
+
+		public static int[] GenerateRandomArray(int size, Random random) {
+			var array = Enumerable.Range(0, size).ToArray();
+			ShuffleArray(array, random);
+			return array;
+		}
+	}
+}
---
+++ ShareClipbrd/ShareClipbrd.Core/Services/RawBitmapDrawer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareClipbrd.Core.Services {
+	public struct Color {
+		public byte r, g, b, a;
+
+		public static Color FromRGB(byte r, byte g, byte b) {
+			return new Color { r = r, g = g, b = b, a = 255 };
+		}
+	}
+
+	public interface IRawBitmapDrawer {
+		void SetPixel(int x, int y, Color color);
+		void Fill(Color color);
+	}
+
+	public class RawBitmapDrawer: IRawBitmapDrawer {
+		private int width;
+		private int height;
+		private IntPtr firstPixelAddr;
+
+		public int Width { get => width; }
+		public int Height { get => height; }
+
+		public RawBitmapDrawer(int width, int height, IntPtr firstPixelAddr) {
+			this.width = width;
+			this.height = height;
+			this.firstPixelAddr = firstPixelAddr;
+		}
+
+		public void SetPixel(int x, int y, Color color) {
+			if (x >= width || y >= height) {
+				return;
+			}
+			unsafe {
+				var ptr = (Color*)firstPixelAddr;
+				*(ptr + x + y * width) = color;
+			}
+		}
+
+		public void Fill(Color color) {
+			var length = width * height;
+			unsafe {
+				var ptr = (Color*)firstPixelAddr;
+				for (int i = 0; i < length; i++) {
+					*ptr = color;
+					ptr++;
+				}
+			}
+		}
+	}
+}
