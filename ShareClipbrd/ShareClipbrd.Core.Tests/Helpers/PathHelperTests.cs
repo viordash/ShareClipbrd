451026267e9e5e5ec8c47db572dec44ea9fbd5bb
Vitaliy Orazov <viordash@mail.ru>
2023-06-17 17:27:29 +0300

    add PathHelper for validate path

---
+++ ShareClipbrd/ShareClipbrd.Core.Tests/Helpers/PathHelperTests.cs
@@ -0,0 +1,21 @@
+﻿using ShareClipbrd.Core.Helpers;
+
+namespace ShareClipbrd.Core.Tests.Helpers {
+    public class PathHelperTests {
+        [Test]
+        public void IsAbsolute_Test() {
+            Assert.True(PathHelper.IsAbsolute("C:\\ShareClipbrd\\ShareClipbrd.Core\\Clipboard\\ClipboardFile.cs"));
+            Assert.True(PathHelper.IsAbsolute("C:\\ShareClipbrd//ClipboardFile.cs"));
+            Assert.True(PathHelper.IsAbsolute("\\tsclient\\Documents.zip"));
+            Assert.True(PathHelper.IsAbsolute("/home/Documents.zip"));
+
+            Assert.False(PathHelper.IsAbsolute("Projects\\ShareClipbrd\\ShareClipbrd\\ShareClipbrd.Core\\Clipboard\\ClipboardFile.cs"));
+            Assert.False(PathHelper.IsAbsolute("Documents"));
+            Assert.False(PathHelper.IsAbsolute("   "));
+            Assert.False(PathHelper.IsAbsolute("/"));
+            Assert.False(PathHelper.IsAbsolute("//"));
+            Assert.False(PathHelper.IsAbsolute("\\"));
+            Assert.False(PathHelper.IsAbsolute("\\\\"));
+        }
+    }
+}
---
+++ ShareClipbrd/ShareClipbrd.Core/Helpers/PathHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace ShareClipbrd.Core.Helpers {
+    public class PathHelper {
+        public static bool IsAbsolute(string path) {
+            try {
+                return Path.IsPathRooted(path) && !string.IsNullOrEmpty(Path.GetDirectoryName(path));
+            } catch {
+                return false;
+            }
+        }
+    }
+}
