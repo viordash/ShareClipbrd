ff1c3530e225b039c6af2c1bb84de972d6fa3101
Vitaliy Orazov <viordash@mail.ru>
2023-08-19 15:30:24 +0300

    wip. add IClipboard

---
+++ ShareClipbrd/ShareClipbrd.Clipboard.Core/ClipboardProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareClipbrd.Clipboard.Core {
+    internal class ClipboardProvider {
+        public IClipboard Current {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ ShareClipbrd/ShareClipbrd.Clipboard.Core/IClipboard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareClipbrd.Clipboard.Core {
+    internal interface IClipboard {
+        Task<string[]> GetFormatsAsync();
+        Task<bool> ContainsFileDropList();
+        Task<bool> ContainsImage();
+
+        Task<object?> GetData(string format);
+    }
+}
