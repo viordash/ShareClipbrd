85c3c299d09054a33076bec0646b2775cac45db2
Vitaliy Orazov <viordash@mail.ru>
2023-08-20 14:45:21 +0300

    wip. X11 specific clipboard lib

---
+++ ShareClipbrd/Clipboard.X11/Clipboard.cs
@@ -0,0 +1,21 @@
+﻿using Clipboard.Core;
+
+namespace Clipboard.OS {
+    internal class Clipboard : IClipboard {
+        public Task<bool> ContainsFileDropList() {
+            return Task.FromResult(false);
+        }
+
+        public Task<bool> ContainsImage() {
+            return Task.FromResult(false);
+        }
+
+        public Task<object> GetData(string format) {
+            return Task.FromResult(new object());
+        }
+
+        public Task<string[]> GetFormatsAsync() {
+            return Task.FromResult(Array.Empty<string>());
+        }
+    }
+}
