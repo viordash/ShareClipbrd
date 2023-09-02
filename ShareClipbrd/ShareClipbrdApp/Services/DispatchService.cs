9d652e8fee783ffdf7ab5736119fee7a58b405f0
Vitaliy Orazov <viordash@mail.ru>
2023-09-03 00:10:18 +0300

    dispatch clipboard data in STA thread

--- ShareClipbrd/ShareClipbrdApp/Services/DispatchService.cs
+++ ShareClipbrd/ShareClipbrdApp/Services/DispatchService.cs
@@ -13,25 +13,27 @@ using ShareClipbrd.Core.Services;
 namespace ShareClipbrdApp.Services {
     public class DispatchService : IDispatchService {
         public async void ReceiveData(ClipboardData clipboardData) {
-            if(clipboardData.Formats.Any()) {
-                Debug.WriteLine($"   *** formats: {string.Join(", ", clipboardData.Formats)}");
+            await Dispatcher.UIThread.InvokeAsync(new Action(async () => {
+                if(clipboardData.Formats.Any()) {
+                    Debug.WriteLine($"   *** formats: {string.Join(", ", clipboardData.Formats)}");
+                    if(Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+                        var clipboard = ClipboardProvider.Get(desktop.MainWindow);
+                        await clipboard.Clear();
+                        await clipboard.SetDataObject(clipboardData);
+                    }
 
-                if(Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
-                    var clipboard = ClipboardProvider.Get(desktop.MainWindow);
-                    await clipboard.Clear();
-                    // await Task.Delay(100);
-                    await clipboard.SetDataObject(clipboardData);
                 }
-            }
+            }));
         }
 
         public async void ReceiveFiles(IList<string> files) {
-            if(Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
-                var clipboard = ClipboardProvider.Get(desktop.MainWindow);
-                await clipboard.Clear();
-                // await Task.Delay(100);
-                await clipboard.SetFileDropList(files);
-            }
+            await Dispatcher.UIThread.InvokeAsync(new Action(async () => {
+                if(Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+                    var clipboard = ClipboardProvider.Get(desktop.MainWindow);
+                    await clipboard.Clear();
+                    await clipboard.SetFileDropList(files);
+                }
+            }));
         }
 
     }
