7235403f204e8e72746c5ea1e63251694dd36433
Vitalii Orazov <viordash@mail.ru>
2023-09-30 22:56:40 +0300

    remove extra wait code

--- ShareClipbrd/ShareClipbrd.Core/Services/DataClient.cs
+++ ShareClipbrd/ShareClipbrd.Core/Services/DataClient.cs
@@ -73,11 +73,11 @@ namespace ShareClipbrd.Core.Services {
 
         public async Task SendFileDropList(StringCollection fileDropList) {
             cts.Cancel();
-            var inProcess = !pingTimer.Enabled;
-            if(inProcess) {
-                Debug.WriteLine("--- inProcess 0");
-                await Task.Delay(1000);
-            }
+            // var inProcess = !pingTimer.Enabled;
+            // if(inProcess) {
+            //     Debug.WriteLine("--- inProcess 0");
+            //     await Task.Delay(1000);
+            // }
             cts = new();
             var cancellationToken = cts.Token;
             try {
@@ -116,11 +116,11 @@ namespace ShareClipbrd.Core.Services {
 
         public async Task SendData(ClipboardData clipboardData) {
             cts.Cancel();
-            var inProcess = !pingTimer.Enabled;
-            if(inProcess) {
-                Debug.WriteLine("--- inProcess 0");
-                await Task.Delay(1000);
-            }
+            // var inProcess = !pingTimer.Enabled;
+            // if(inProcess) {
+            //     Debug.WriteLine("--- inProcess 0");
+            //     await Task.Delay(1000);
+            // }
             cts = new();
             var cancellationToken = cts.Token;
             try {
