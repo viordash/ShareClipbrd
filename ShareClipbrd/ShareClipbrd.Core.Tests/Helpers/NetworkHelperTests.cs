864d36afd0b7f7a0c79ec115aa2032f4d9a89041
Vitaliy Orazov <viordash@mail.ru>
2025-03-05 23:36:32 +0300

    NetworkHelper. improve parsing hostname

--- ShareClipbrd/ShareClipbrd.Core.Tests/Helpers/NetworkHelperTests.cs
+++ ShareClipbrd/ShareClipbrd.Core.Tests/Helpers/NetworkHelperTests.cs
@@ -23,7 +23,7 @@ namespace ShareClipbrd.Core.Tests.Helpers {
 
         [Test]
         public void ResolveHostName_IPv6_Any_Test() {
-            var adr = NetworkHelper.ResolveHostName(":::55542");
+            var adr = NetworkHelper.ResolveHostName("[::]:55542");
             Assert.That(adr.AddressFamily, Is.EqualTo(AddressFamily.InterNetworkV6));
             Assert.That(adr.Address, Is.EqualTo(IPAddress.IPv6Any));
             Assert.That(adr.Port, Is.EqualTo(55542));
@@ -35,6 +35,9 @@ namespace ShareClipbrd.Core.Tests.Helpers {
             Assert.That(adr.AddressFamily, Is.EqualTo(AddressFamily.InterNetwork));
             Assert.That(adr.Address, Is.EqualTo(IPAddress.Loopback));
             Assert.That(adr.Port, Is.EqualTo(55542));
+            adr = NetworkHelper.ResolveHostName(":12345");
+            Assert.That(adr.AddressFamily, Is.EqualTo(AddressFamily.InterNetwork));
+            Assert.That(adr.Port, Is.EqualTo(12345));
         }
 
         [Test]
@@ -59,29 +62,29 @@ namespace ShareClipbrd.Core.Tests.Helpers {
         }
 
         [Test]
-        public void ResolveHostName_No_Port_Throws_ArgumentException() {
-            var exception = Assert.Throws<ArgumentException>(() => NetworkHelper.ResolveHostName("localhost"));
-            Assert.That(exception?.Message, Is.EqualTo("Port not valid, hostname: localhost"));
-            exception = Assert.Throws<ArgumentException>(() => NetworkHelper.ResolveHostName("127.0.0.1"));
-            Assert.That(exception?.Message, Is.EqualTo("Port not valid, hostname: 127.0.0.1"));
-            exception = Assert.Throws<ArgumentException>(() => NetworkHelper.ResolveHostName("127.0.0.1:"));
-            Assert.That(exception?.Message, Is.EqualTo("Port not valid, hostname: 127.0.0.1:"));
-            exception = Assert.Throws<ArgumentException>(() => NetworkHelper.ResolveHostName("[::1]"));
-            Assert.That(exception?.Message, Is.EqualTo("Port not valid, hostname: [::1]"));
-            exception = Assert.Throws<ArgumentException>(() => NetworkHelper.ResolveHostName("[fe80::9656:d028:8652:66b6]"));
-            Assert.That(exception?.Message, Is.EqualTo("Port not valid, hostname: [fe80::9656:d028:8652:66b6]"));
-            exception = Assert.Throws<ArgumentException>(() => NetworkHelper.ResolveHostName(""));
-            Assert.That(exception?.Message, Is.EqualTo("Port not valid, hostname: "));
+        public void ResolveHostName_No_Port_returns_zero_port() {
+            var adr = NetworkHelper.ResolveHostName("localhost");
+            Assert.That(adr.Port, Is.EqualTo(0));
+            adr = NetworkHelper.ResolveHostName("127.0.0.1");
+            Assert.That(adr.Port, Is.EqualTo(0));
+            adr = NetworkHelper.ResolveHostName("[::]");
+            Assert.That(adr.Port, Is.EqualTo(0));
+            adr = NetworkHelper.ResolveHostName(":0");
+            Assert.That(adr.Port, Is.EqualTo(0));
+            adr = NetworkHelper.ResolveHostName("[fe80::9656:d028:8652:66b6]");
+            Assert.That(adr.Port, Is.EqualTo(0));
+            adr = NetworkHelper.ResolveHostName("[::1]");
+            Assert.That(adr.Port, Is.EqualTo(0));
         }
 
         [Test]
-        public void ResolveHostName_Invalid_Port_Throws_ArgumentException() {
-            Assert.Throws<ArgumentOutOfRangeException>(() => NetworkHelper.ResolveHostName("localhost:-1"));
-            Assert.Throws<ArgumentOutOfRangeException>(() => NetworkHelper.ResolveHostName(":-1"));
-            Assert.Throws<ArgumentOutOfRangeException>(() => NetworkHelper.ResolveHostName("-1"));
-            Assert.Throws<ArgumentOutOfRangeException>(() => NetworkHelper.ResolveHostName("localhost:65536"));
-            Assert.Throws<ArgumentOutOfRangeException>(() => NetworkHelper.ResolveHostName(":65536"));
-            Assert.Throws<ArgumentOutOfRangeException>(() => NetworkHelper.ResolveHostName("65536"));
+        public void ResolveHostName_Invalid_Port_Throws_Exception() {
+            Assert.Throws<ArgumentException>(() => NetworkHelper.ResolveHostName("localhost:-1"));
+            Assert.Throws<ArgumentException>(() => NetworkHelper.ResolveHostName(":-1"));
+            Assert.Throws<SocketException>(() => NetworkHelper.ResolveHostName("-1"));
+            Assert.Throws<ArgumentException>(() => NetworkHelper.ResolveHostName("localhost:65536"));
+            Assert.Throws<ArgumentException>(() => NetworkHelper.ResolveHostName(":65536"));
+            Assert.Throws<ArgumentException>(() => NetworkHelper.ResolveHostName("127.0.0.1:"));
         }
 
         [Test]
--- ShareClipbrd/ShareClipbrd.Core/Helpers/NetworkHelper.cs
+++ ShareClipbrd/ShareClipbrd.Core/Helpers/NetworkHelper.cs
@@ -1,21 +1,41 @@
-﻿using System.Net;
+﻿using System.Globalization;
+using System.Net;
 using System.Net.Sockets;
 
 namespace ShareClipbrd.Core.Helpers {
     public class NetworkHelper {
-        public static IPEndPoint ResolveHostName(string hostname) {
-            int portStart = hostname.LastIndexOf(':');
-            if(!int.TryParse(hostname[(portStart + 1)..], out int port)) {
-                throw new ArgumentException($"Port not valid, hostname: {hostname}");
-            }
-            var ipString = hostname[..portStart];
-            if(ipString == IPAddress.Any.ToString()) {
-                return new IPEndPoint(IPAddress.Any, port);
-            }
-            if(ipString == IPAddress.IPv6Any.ToString()) {
-                return new IPEndPoint(IPAddress.IPv6Any, port);
+        public static int ExtractPort(string url, out int startPos) {
+            startPos = url.LastIndexOf(':');
+            int port;
+            if(startPos < 0) {
+                startPos = url.Length;
+                return 0;
             }
 
+            try {
+                port = int.Parse(url[(startPos + 1)..], NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture);
+                _ = new IPEndPoint(IPAddress.Any, port);
+                return port;
+            } catch(ArgumentOutOfRangeException) {
+                throw new ArgumentException($"Port not valid");
+            } catch(FormatException) {
+                throw new ArgumentException($"Port not valid");
+            }
+        }
+
+        public static IPEndPoint ResolveHostName(string hostname) {
+            var isIpEndPoint = IPEndPoint.TryParse(hostname, out var ipEndPoint);
+            if(isIpEndPoint && ipEndPoint != null) {
+                return ipEndPoint;
+            }
+
+            var isIpAddress = IPAddress.TryParse(hostname, out var ipAdress);
+            var port = ExtractPort(hostname, out int portStart);
+            if(isIpAddress && ipAdress != null) {
+                return new IPEndPoint(ipAdress, port); ;
+            }
+
+            var ipString = hostname[..portStart].Trim();   
             var addresses = Dns.GetHostAddresses(ipString);
             var adr = addresses.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork
                     && !IPAddress.IsLoopback(x));
